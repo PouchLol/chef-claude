628012266eacac7f8c4caf648eec1916ea7e10aa
Karthik <karthikh2004@gmail.com>
2025-01-13 21:40:49 +0530

    create an input form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/Navbar";
+import Input from "./components/Input"
 
 export default function App(){
     return(
         <>
         <NavBar/>
+        <Input/>
         </>
     )
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+import "./input.css"
+
+export default function Input(){
+    return (
+        <form className="body-form">
+            <input aria-label="add-ingredient" className="body-input" placeholder="eg: oregano"></input>
+            <button className="body-button"> + Add ingredient!</button>
+        </form>
+    )
+}
