71a5f54302f8e0bfced60d0bd053e4db15414241
Karthik <karthikh2004@gmail.com>
2025-01-13 23:20:49 +0530

    first event listener in react

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,7 @@
 import "./input.css"
 
 export default function Input(){
+    
     return (
         <form className="body-form">
             <input aria-label="add-ingredient" className="body-input" placeholder="eg: oregano"></input>
