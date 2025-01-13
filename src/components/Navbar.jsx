55d77b928488638edabbd9be635fcad88c74c7a0
Karthik <karthikh2004@gmail.com>
2025-01-13 21:23:29 +0530

    create app.jsx, create a folder for components and create a navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./navbar.css"
+
+export default function NavBar(){
+    return (
+        <>
+        <nav className="navbar">
+            <img src="chef.png" className="logo"></img>
+            <h2 className="brand-name">Chef Claude</h2>
+        </nav>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
+import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import App from "./App"
+import "./main.css"
 
 
 const rootEl = createRoot(document.getElementById("root"))
 
 rootEl.render(
-    <h1>Hello world!</h1>
+    <StrictMode>
+        <App />
+    </StrictMode>
 )
